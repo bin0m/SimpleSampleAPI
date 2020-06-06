c6512fe6717ffa292a8e18c73f79d7eb6289eda2
Gregory Levit <grigoriy.levit@arcadia.spb.ru>
2020-06-07 01:55:08 +0300

    Added EF Core

--- SimpleSampleAPI/Startup.cs
+++ SimpleSampleAPI/Startup.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SimpleSampleAPI.Models;
 
 namespace SimpleSampleAPI
 {
@@ -24,6 +27,8 @@ namespace SimpleSampleAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("ToDoInMemory"));
             services.AddControllers();
         }
 
