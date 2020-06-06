acbbb8f5e889b942f22b5fc50bcdb83c5fe3d04f
Gregory Levit <grigoriy.levit@arcadia.spb.ru>
2020-06-07 02:02:50 +0300

    Add Controller and refactoring

--- SimpleSampleAPI/Models/TodoItem.cs
+++ SimpleSampleAPI/Models/TodoItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SimpleSampleAPI.Models
+﻿namespace SimpleSampleAPI.Models
 {
     public class TodoItem
     {
