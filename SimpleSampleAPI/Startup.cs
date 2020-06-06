6c1a6bf18eb45f770014152412099e9e5fe68b79
Gregory Levit <grigoriy.levit@arcadia.spb.ru>
2020-06-07 02:49:31 +0300

    Added Swagger

--- SimpleSampleAPI/Startup.cs
+++ SimpleSampleAPI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SimpleSampleAPI.Models;
 
 namespace SimpleSampleAPI
@@ -23,6 +24,7 @@ namespace SimpleSampleAPI
             services.AddDbContext<TodoContext>(opt =>
                 opt.UseInMemoryDatabase("ToDoInMemory"));
             services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "SimpleSample Base API", Version = "v1"}));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +42,12 @@ namespace SimpleSampleAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleSample V1");
             });
         }
     }
